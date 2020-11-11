d5e954f29b4f752e4eb3efa34627c1fc48138eb1
benjamin.kelly <benjamin.kelly@espas.ch>
2020-11-11 16:39:00 +0100

    Divided App into "Login" and "Entries" portions.

---
+++ src/EntriesView.js
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react';
+
+export default function EntriesView() {
+
+    const [entries, setEntries] = useState([]);
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
